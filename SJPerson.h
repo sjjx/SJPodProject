35be04471bf01b4aa9c694d324fe27ca06d41ee3
SJ <wusijun@tuandai.com>
2018-11-14 10:06:02 +0800

    SJPodPoject init
    Signed-off-by: SJ <wusijun@tuandai.com>

---
+++ SJPerson.h
@@ -0,0 +1,16 @@
+//
+//  SJPerson.h
+//  Pods
+//
+//  Created by 吴四军 on 2018/11/14.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface SJPerson : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ SJPerson.m
@@ -0,0 +1,12 @@
+//
+//  SJPerson.m
+//  Pods
+//
+//  Created by 吴四军 on 2018/11/14.
+//
+
+#import "SJPerson.h"
+
+@implementation SJPerson
+
+@end
